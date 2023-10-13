db0ca75e1511e5d23005b9841919ae890c4bd836
Von Coleman <von.coleman@yahoo.com>
2023-10-12 20:48:17 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App { //The name of the class is App. The class is public.
+    public static void main(String[] args){ //Main. Like, my main man.The program entry point. The beginning!
+        System.out.println("Wake up, Von"); //System.out.println is the line of code needed to print/display the message in parenthesis.
+    }
+}
